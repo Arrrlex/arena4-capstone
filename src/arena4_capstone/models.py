831fdd761cc7060d922f1f4257d80aa89d17c461
Gergely Kiss <32220585+madasigon@users.noreply.github.com>
2025-03-14 18:00:34 +0000

    refactor model name

--- src/arena4_capstone/models.py
+++ src/arena4_capstone/models.py
@@ -12,6 +12,11 @@ hf.login(token=settings.HF_API_TOKEN)
 if device.type == "cuda":
     t.cuda.empty_cache()
 
-gemma = LanguageModel(
+gemma_2_2b_it: LanguageModel = LanguageModel(
     "google/gemma-2-2b-it", device_map=device, token=settings.HF_API_TOKEN
 )
+
+gemma_2_9b_it: LanguageModel = LanguageModel(
+    "google/gemma-2-9b-it", device_map=device, token=settings.HF_API_TOKEN
+)
+
--- src/arena4_capstone/tokens.py
+++ src/arena4_capstone/tokens.py
@@ -1,6 +1,6 @@
-from arena4_capstone.models import gemma
+from arena4_capstone.models import gemma_2_2b_it
 
-eos = str(gemma.tokenizer.eos_token)
-bos = str(gemma.tokenizer.bos_token)
+eos = str(gemma_2_2b_it.tokenizer.eos_token)
+bos = str(gemma_2_2b_it.tokenizer.bos_token)
 sot = "<start_of_turn>"
 eot = "<end_of_turn>"
